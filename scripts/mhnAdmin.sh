b313be62a8029ac680bb914b6da909dd5544bc40
Artem <artemskitenko93@gmail.com>
2018-11-18 22:18:28 -0600

    fix

--- scripts/mhnAdmin.sh
+++ scripts/mhnAdmin.sh
@@ -2,9 +2,6 @@
 # Run this script from mhn-admin VM shell
 sudo apt-get install git -y
 
-# Install mysql-server to get "replace" bash tool
-sudo apt-get install mysql-server
-
 cd /opt
 sudo git clone https://github.com/RedolentSun/mhn.git
 cd mhn
@@ -14,7 +11,7 @@ cd scripts/
 #     ```pip install -e git+https://github.com/HurricaneLabs/pyev.git#egg=pyev```  
 #     to  
 #     ```pip install -e git+https://github.com/couozu/pyev.git#egg=pyev```
-replace "HurricaneLabs" "couozu" -- install_hpfeeds.sh
+ex -s -c '%s/HurricaneLabs/couozu/g|x' install_hpfeeds.sh
 cd ../
 
 sudo ./install.sh
